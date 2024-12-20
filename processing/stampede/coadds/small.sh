7f70f472bfdb9c02deaa81a62f16079d435e826d
Steven Stetzler <steven.stetzler@gmail.com>
2024-12-19 19:18:22 -0800

    update resources

--- processing/stampede/coadds/small.sh
+++ processing/stampede/coadds/small.sh
@@ -17,8 +17,8 @@ export PROC_LSST_QUEUE="multi" # ${PROC_LSST_QUEUE:-"multi"}
 export PROC_LSST_MULTI_QUEUES="local,skx" # ${PROC_LSST_MULTI_QUEUES:-"local,skx"}
 export PROC_LSST_NODES_PER_BLOCK="1" #${PROC_LSST_NODES_PER_BLOCK:-1}
 export PROC_LSST_CORES_PER_NODE="32" # ${PROC_LSST_CORES_PER_NODE:-48}
-export PROC_LSST_MAX_BLOCKS="20" # ${PROC_LSST_MAX_BLOCKS:-1}
-export J=30 # cores per worker for local
+export PROC_LSST_MAX_BLOCKS="5" # ${PROC_LSST_MAX_BLOCKS:-1}
+export J=6 # cores per worker for local
 env | grep PROC_LSST
 
 function cleanup() {
@@ -34,7 +34,7 @@ $w/bin/db_ctl.sh $REPO start
 # patches=$(python $w/bin/select_patches.py $w/data/warp_counts_allSky.csv --min 0 --max 50)
 # python $w/bin/pipeline.py $REPO coadd allSky --steps assembleCoadd --where "skymap='discrete' and patch in (${patches})"
 # python $w/bin/coadd.py $REPO allSky --where "skymap='discrete' and patch in (${patches})"
-python $w/bin/coadd_subsets.py $REPO allSky $w/data/warp_subsets --subsets "0|1|2|3|4|5"
+python $w/bin/coadd_subsets.py $REPO allSky $w/data/warp_subsets --subsets "0|1|2|3|4|5" --workers 5
 
 $w/bin/db_ctl.sh $REPO stop
 
