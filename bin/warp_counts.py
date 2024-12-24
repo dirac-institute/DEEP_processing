23f49cc6a27d310eb4cc630d68e662dfbbe76db8
Steven Stetzler <steven.stetzler@gmail.com>
2024-12-24 15:32:02 -0800

    per-band warp counts

--- bin/warp_counts.py
+++ bin/warp_counts.py
@@ -11,35 +11,54 @@ def main():
     parser.add_argument("--collections", nargs="+", default=["*"])
     parser.add_argument("--output", default=sys.stdout)
     parser.add_argument("--format", default="ascii.fast_csv")
-    parser.add_argument("--datasetType", default="deepCoadd_directWarp")
+    parser.add_argument("--coadd-type", default="deepCoadd")
 
     args = parser.parse_args()
 
     butler = dafButler.Butler(args.repo, writeable=True)
 
     collections = butler.registry.queryCollections(args.collections)
-
-    refs = list(set(list(butler.registry.queryDatasets(args.datasetType, collections=collections))))
-    patches = {}
-    for ref in refs:
-        patches[ref.dataId['patch']] = patches.get(ref.dataId['patch'], 0) + 1
     
-    # grouped_by_count = {}
-    # for k, v in patches.items():
-    #     grouped_by_count[v] = grouped_by_count.get(v, [])
-    #     grouped_by_count[v].append(k)
+    # loop through refs of directWarp and psfMatchedWarp
+    # group by patch and band
+    # exclude patch/band combinations where n(directWarp) != n(psfMatchedWarp)
+    # print (patch, band, n)
+
+    direct_dataset = f"{args.coadd_type}_directWarp"
+    direct_refs = set(list(butler.registry.queryDatasets(direct_dataset, collections=collections)))
+    psfMatched_dataset = f"{args.coadd_type}_psfMatchedWarp"
+    psfMatched_refs = set(list(butler.registry.queryDatasets(psfMatched_dataset, collections=collections)))
+
+    def count_refs(refs, keys=['patch', 'band']):
+        counts = {}
+        for ref in refs:
+            k = (ref.dataId['patch'], ref.dataId['band'])
+            counts[k] = counts.get(k, 0) + 1
+        return counts
+
+    direct_patches = count_refs(direct_refs)
+    psfMatched_patches = count_refs(psfMatched_refs)
+
+    patches = set(list(direct_patches.keys())).intersection(set(list(psfMatched_patches.keys())))
 
     outputs = []
-    for k, v in patches.items():
+    for k in patches:
+        patch, band = k
+        n_direct = direct_patches[k]
+        n_psfMatched = psfMatched_patches[k]
+        if n_direct != n_psfMatched:
+            print("number of direct and psfMatched refs do not match for patch, band", patch, band, file=sys.stderr)
+            continue
         outputs.append(
             {
-                "patch": k,
-                "n": v,
+                "patch": patch,
+                "band": band,
+                "n": n_direct,
             }
         )
 
     outputs = astropy.table.Table(outputs)
-    if args.output != sys.stdout and args != sys.stderr:
+    if args.output != sys.stdout and args.output != sys.stderr:
         Path(args.output).parent.mkdir(parents=True, exist_ok=True)
     outputs.write(args.output, format=args.format)
 
