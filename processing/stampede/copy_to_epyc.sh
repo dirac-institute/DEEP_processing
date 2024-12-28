188454a9850425cf2ee17a9fe5f32663cd13c8a6
Steven Stetzler <steven.stetzler@gmail.com>
2024-12-27 18:16:33 -0800

    copy

--- processing/stampede/copy_to_epyc.sh
+++ processing/stampede/copy_to_epyc.sh
@@ -5,4 +5,4 @@ cd $w
 source $w/bin/setup.sh
 
 runs=$(butler query-collections $REPO --collection-type RUN | grep RUN | awk '{print $1}')
-echo "${runs}" | xargs -P 48 -I % python bin/transfer.py ./repo/% epyc /epyc/data4/stampede_repo/% --exclude "raw" --exclude "*postISRCCD*" --exclude "*icExp*" --exclude "deepCoadd*Warp" --exclude "overscanRaw" --exclude "cpBiasIsrExp" --exclude "cpFlatIsrExp" 1> transfer_$(date +%S).log 2>&1
+echo "${runs}" | xargs -P 48 -I % python bin/transfer.py ./repo/% epyc /epyc/data4/stampede_repo/% --exclude "raw" --exclude "*postISRCCD*" --exclude "*icExp*" --exclude "deepCoadd*Warp" --exclude "overscanRaw" --exclude "cpBiasIsrExp" --exclude "cpFlatIsrExp" 1> transfer_$(date +%s).log 2>&1
