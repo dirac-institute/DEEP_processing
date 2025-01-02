3e3e5dcc9ccc0e6d720c1556bb0332e9e4dbe843
stevenstetzler <steven.stetzler@gmail.com>
2025-01-02 12:25:43 -0600

    final

---
+++ processing/final.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#SBATCH --partition=skx
+#SBATCH --time=4:00:00
+#SBATCH --nodes=1
+
+w=$DEEP_PROJECT_DIR
+cd $w
+source $w/bin/setup.sh
+
+function cleanup() {
+    $w/bin/db_ctl.sh $w/repo stop
+}
+
+trap cleanup SIGINT
+trap cleanup SIGTERM
+trap cleanup SIGEXIT
+
+./bin/db_ctl.sh ./repo start
+
+
+dirname -- $@ | xargs -P 48 -I % bash %/final_job.bash %/*Z.qgraph $REPO
+
+cleanup
+
+
