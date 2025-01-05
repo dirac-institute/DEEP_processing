c670b956d4be073780b3bc3ac833dc25bb4d790c
stevenstetzler <steven.stetzler@gmail.com>
2025-01-04 21:26:01 -0600

    coadds

--- processing/stampede/coadds/all.sh
+++ processing/stampede/coadds/all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#SBATCH --partition=skx-dev
-#SBATCH --time=2:00:00
+#SBATCH --partition=skx
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 
 # COADD MEDIUM - 150 < n <= 200
@@ -14,7 +14,7 @@ source $w/bin/setup.sh
 
 export PROC_LSST_SITE="stampede" #${PROC_LSST_SITE:-"stampede"}
 export PROC_LSST_QUEUE="multi" # ${PROC_LSST_QUEUE:-"multi"}
-export PROC_LSST_MULTI_QUEUES="skx" # ${PROC_LSST_MULTI_QUEUES:-"local,skx"}
+export PROC_LSST_MULTI_QUEUES="local,skx" # ${PROC_LSST_MULTI_QUEUES:-"local,skx"}
 export PROC_LSST_NODES_PER_BLOCK="1" #${PROC_LSST_NODES_PER_BLOCK:-1}
 export PROC_LSST_CORES_PER_NODE="16" # ${PROC_LSST_CORES_PER_NODE:-48}
 export PROC_LSST_MAX_BLOCKS="4" # ${PROC_LSST_MAX_BLOCKS:-1}
